078bce69382d1fb46ebab986ced3dc2f869bfa90
RxPro <wxweb@mail.ru>
2022-11-09 15:31:50 +0300

    Правки из-за смены дизайна. Завершение верстки

--- sources/src/js/_libs_concat.cjs
+++ sources/src/js/_libs_concat.cjs
@@ -1,4 +1,5 @@
 module.exports = [ // comma separated
    "./src/libs/jquery/dist/jquery.min.js",
    "./src/libs/swiper/swiper-bundle.min.js",
+   "./src/libs/select2/dist/js/select2.min.js",
 ];
--- sources/src/js/main.js
+++ sources/src/js/main.js
@@ -4,6 +4,12 @@ $(document).ready(function () {
    // Your code here
    sliderSwipe1()
    sliderSwipe2()
+   regionList()
+   modalShow()
+   formInput()
+   contentShow()
+   contentSlide()
+   blockquoteText()
 });
 
 // Your functions here
@@ -47,3 +53,90 @@ function sliderSwipe2() {
       },
    });
 }
+
+function regionList() {
+   $('.region').select2({
+      placeholder: "Регион",
+   });
+}
+
+function modalShow() {
+   $('.js-login-btn').on('click', function (e) {
+      e.preventDefault()
+      $('.modal-login').addClass('modal--active')
+      $('.overlay').addClass('overlay--active')
+      $('body').addClass('no-scroll')
+   })
+
+   $('.js-credit-btn').on('click', function (e) {
+      e.preventDefault()
+      $('.modal-credit').addClass('modal--active')
+      $('.overlay').addClass('overlay--active')
+      $('body').addClass('no-scroll')
+   })
+
+   $('.modal__close').on('click', function (e) {
+      e.preventDefault()
+      $(this).parent('.modal').removeClass('modal--active')
+      $('.overlay').removeClass('overlay--active')
+      $('body').removeClass('no-scroll')
+   })
+
+   $('.overlay').on('click', function() {
+      $('.modal').removeClass('modal--active')
+      $('.overlay').removeClass('overlay--active')
+      $('body').removeClass('no-scroll')
+   })
+}
+
+function blockquoteText() {
+   $('.blockquote__read').on('click', function(e) {
+      let $this = $(this)
+
+      e.preventDefault()
+      $this.toggleClass('blockquote__read--toggled')
+      $this.parent().find('.blockquote__text').toggleClass('blockquote__text--all')
+   })
+}
+
+function formInput() {
+   $('input').on('change', function () {
+      if (this.value.length) {
+         this.classList.add('full');
+      } else {
+         this.classList.remove('full');
+      }
+   })
+}
+
+function contentShow() {
+   $('.content__top').click(function (e) {
+      e.preventDefault();
+
+      let $this = $(this);
+
+      if ($this.next().hasClass('show')) {
+         $this.next().removeClass('show')
+         $this.next().slideUp(350)
+         $this.find('.content__toggle').removeClass('content__toggle--active')
+      } else {
+         $this.find('.content__toggle').addClass('content__toggle--active')
+         $this.parent().find('.num-list').removeClass('show')
+         $this.parent().find('.num-list').slideUp(350)
+         $this.next().toggleClass('show')
+         $this.next().slideToggle(350)
+      }
+   });
+}
+
+function contentSlide() {
+   $('.js-anchor[href^="#"]').bind("click", function (e) {
+      var anchor = $(this);
+      $('html, body').stop().animate({
+         scrollTop: $(anchor.attr('href')).offset().top - -10
+      }, 800);
+      e.preventDefault();
+   });
+   return false;
+}
+
